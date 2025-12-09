fe1946b9783248f03daac0c0764adedc5b9dad8c
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-12-09 15:37:47 +0100

    added the http

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideStore()
-]
+    provideHttpClient(),
+    provideStore(),
+  ],
 };
