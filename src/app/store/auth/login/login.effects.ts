258aa6c29c7d56f92b5e4a6efb9131d90e30f6c9
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-12-11 19:40:11 +0100

    added the login effect

--- src/app/store/auth/login/login.effects.ts
+++ src/app/store/auth/login/login.effects.ts
@@ -1,10 +1,43 @@
-import { inject } from "@angular/core";
-import { AuthService } from "../../../core/services/users/auth.service";
+import { inject, Injectable } from '@angular/core';
+import { AuthService } from '../../../core/services/users/auth.service';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Router } from '@angular/router';
+import * as LoginActions from '../login/login.actions';
+import { catchError, map, mergeMap, of, tap } from 'rxjs';
 
-@injectable()
+@Injectable()
 export class LoginEffects {
-  private actions$ = inject(Actions)
-    private authService = inject(AuthService);
+  private actions$ = inject(Actions);
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
-    loginRequest$ = createEffect(() =>
+  loginRequest$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(LoginActions.loginUser),
+      mergeMap(({ credentials }) =>
+        this.authService.login(credentials.email, credentials.password).pipe(
+          map((loginResponse) => {
+            if (loginResponse.error) {
+              return LoginActions.loginUserFailure({ error: loginResponse.messasge });
+            }
+            return LoginActions.loginUserSuccess({ loginResponse });
+          }),
+          catchError((error) =>
+            of(LoginActions.loginUserFailure({ error: error.error?.message || 'Login failed' }))
+          )
+        )
+      )
+    )
+  );
+
+  loginSuccess$ = createEffect(() =>
+      this.actions$.pipe(
+        ofType(LoginActions.loginUserSuccess),
+        tap(({ loginResponse }) => {
+          localStorage.setItem('token', loginResponse.token);
+          this.router.navigate(['/home']);
+        })
+      ),
+    { dispatch: false }
+  );
 }
