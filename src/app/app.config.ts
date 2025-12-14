8ffa232b02a3295721d825566c6bb5dfbc6fbd46
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-12-14 16:29:57 +0100

    added the effect config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,21 @@ import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideHttpClient } from '@angular/common/http';
 import { provideEffects } from '@ngrx/effects';
+import { authReducer } from './store/auth/shared state/auth.reducer';
+import { LoginEffects } from './store/auth/login/login.effects';
+import { RegisterEffects } from './store/auth/register/register.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
-    provideStore(),
-    provideEffects()
-],
+    provideStore({
+      auth: authReducer,
+    }),
+    provideEffects([
+      LoginEffects,
+      RegisterEffects
+    ]),
+  ],
 };
