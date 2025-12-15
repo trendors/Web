5d8a490de9897eee807d4ab84d03cf7ae4aa0b36
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-12-15 14:36:47 +0100

    login logic when i submit and all

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { Store } from '@ngrx/store';
+import { selectAuthError, selectIsLoading } from '../../../store/auth/shared state/auth.selector';
+import { LoginActions } from '../../../store/auth/login/login.actions';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  private fb = inject(FormBuilder);
+  private store = inject(Store);
+
+  isLoading$ = this.store.select(selectIsLoading);
+  error$ = this.store.select(selectAuthError);
+  hidePassword = true;
+
+  loginForm = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.store.dispatch(
+        LoginActions.loginRequest({
+          email: email!,
+          password: password!,
+        })
+      );
+    }
+  }
+}
