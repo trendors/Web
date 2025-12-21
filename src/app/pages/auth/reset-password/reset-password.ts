deff4ccb05569f58f7c6ec6a12778d5b045bbbd5
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-12-21 14:13:26 +0100

    added the reset password logic

--- src/app/pages/auth/reset-password/reset-password.ts
+++ src/app/pages/auth/reset-password/reset-password.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectAuthError, selectIsLoading } from '../../../store/auth/shared state/auth.selector';
+import { PasswordRecoveryActions } from '../../../store/auth/passwordRecovery/password-recovery.actions';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    RouterModule,
+  ],
   templateUrl: './reset-password.html',
   styleUrl: './reset-password.scss',
 })
 export class ResetPassword {
+  private fb = inject(FormBuilder);
+  private store = inject(Store);
+  private route = inject(ActivatedRoute);
 
+  isLoading$ = this.store.select(selectIsLoading);
+  error$ = this.store.select(selectAuthError);
+
+  token: string | null = null;
+
+  resetForm = this.fb.group({
+    newPassword: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.token = params['token'] || null;
+    });
+  }
+
+  onSubmit() {
+    if (this.resetForm.valid && this.token) {
+      this.store.dispatch(
+        PasswordRecoveryActions.resetPasswordRequest({
+          token: this.token,
+          newPassword: this.resetForm.value.newPassword!,
+        })
+      );
+    } else if (!this.token) {
+      alert('Invalid or missing token.');
+    }
+  }
 }
