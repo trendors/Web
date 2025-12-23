76e51e90b54805c753b279cb6a9d15b48dcccd1c
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-12-23 06:52:40 +0100

    space

--- src/app/store/auth/passwordRecovery/password-recovery.actions.ts
+++ src/app/store/auth/passwordRecovery/password-recovery.actions.ts
@@ -1,15 +1,18 @@
-import { createActionGroup, props } from "@ngrx/store";
-import { ForgetPasswordResponse, PasswordResetResponse } from "../../../core/models/users/user.model";
+import { createActionGroup, props } from '@ngrx/store';
+import {
+  ForgotPasswordResponse,
+  PasswordResetResponse,
+} from '../../../core/models/users/user.model';
 
 export const PasswordRecoveryActions = createActionGroup({
-    source: 'Auth Password Recovery',
-    events: {
-        'Forget Password Request': props<{email: string}>(),
-        'Forget Password Success': props<{response: ForgetPasswordResponse}>(),
-        'Forget Password Failure': props<{error: string}>(),
+  source: 'Auth Password Recovery',
+  events: {
+    'Forgot Password Request': props<{ email: string }>(),
+    'Forgot Password Success': props<{ response: ForgotPasswordResponse }>(),
+    'Forgot Password Failure': props<{ error: string }>(),
 
-        'Reset Password Request': props<{token: string, newPassword: string}>(),
-        'Reset Password Success': props<{response: PasswordResetResponse}>(),
-        'Reset Password Failure': props<{error: string}>(),
-    }
-}) 
+    'Reset Password Request': props<{ token: string; newPassword: string }>(),
+    'Reset Password Success': props<{ response: PasswordResetResponse }>(),
+    'Reset Password Failure': props<{ error: string }>(),
+  },
+});
