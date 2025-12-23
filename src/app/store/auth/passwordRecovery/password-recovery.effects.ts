888c6efd1370f0dc229ae3af3d738276f95d9cb8
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-12-23 06:54:34 +0100

    added the effect for the recovery

---
+++ src/app/store/auth/passwordRecovery/password-recovery.effects.ts
@@ -0,0 +1,70 @@
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { AuthService } from '../../../core/services/users/auth.service';
+import { PasswordRecoveryActions } from './password-recovery.actions';
+import { catchError, map, mergeMap, of } from 'rxjs';
+
+@Injectable()
+export class PasswordRecoveryEffects {
+  private actions$ = inject(Actions);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  forgetPassword$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(PasswordRecoveryActions.forgotPasswordRequest),
+      mergeMap(({ email }) =>
+        this.authService.forgotPassword({ email }).pipe(
+          map((response) => {
+            if (response.error) {
+              return PasswordRecoveryActions.forgotPasswordFailure({ error: response.message });
+            }
+            return PasswordRecoveryActions.forgotPasswordSuccess({ response });
+          }),
+          catchError((error) =>
+            of(
+              PasswordRecoveryActions.forgotPasswordFailure({
+                error: error.error?.message || 'Forgot password request failed',
+              })
+            )
+          )
+        )
+      )
+    )
+  );
+
+  resetPassword$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(PasswordRecoveryActions.resetPasswordRequest),
+      mergeMap(({ token, newPassword, }) =>
+        this.authService.resetPassword({ token, newPassword }).pipe(
+          map((response) => {
+            if (response.error) {
+              return PasswordRecoveryActions.resetPasswordFailure({ error: response.message });
+            }
+            return PasswordRecoveryActions.resetPasswordSuccess({ response });
+          }),
+          catchError((error) =>
+            of(
+              PasswordRecoveryActions.resetPasswordFailure({
+                error: error.error?.message || 'Reset password request failed',
+              })
+            )
+          )
+        )
+      )
+    )
+  );
+
+  resetSucessNavigate$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(PasswordRecoveryActions.resetPasswordSuccess),
+        map(() => {
+          this.router.navigate(['/login']);
+        })
+      ),
+    { dispatch: false }
+  );
+}
