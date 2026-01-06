7561363daac5cd89e5157ffd88bd45b28e33bcce
Igboanu Uchenna <igboanuuchenna@gmail.com>
2026-01-06 17:28:45 +0100

    post ngrx reducer

--- src/app/store/posts/post/posts.reducer.ts
+++ src/app/store/posts/post/posts.reducer.ts
@@ -1,40 +1,50 @@
-import { createReducer, on } from "@ngrx/store";
-import { Post } from "../../../core/models/posts/post.model";
-import { PostActions } from "./posts.actions";
+import { createReducer, on } from '@ngrx/store';
+import { Post } from '../../../core/models/posts/post.model';
+import { PostActions } from './posts.actions';
 
 export interface PostsState {
-    items: Post[];
-    totalCount: number;
-    loading: boolean;
-    error: string | null;
+  list: Post[];
+  totalCount: number;
+  loading: boolean;
+  loadingMore: boolean;
+  error: string | null;
 }
 
 export const initialState: PostsState = {
-    items: [],
-    totalCount: 0,
-    loading: false,
-    error: null
+  list: [],
+  totalCount: 0,
+  loading: false,
+  loadingMore: false,
+  error: null,
 };
 
 export const postsReducer = createReducer(
-    initialState,
+  initialState,
 
-    on(PostActions.loadFeed, (state) => ({ 
-        ...state, 
-        loading: true, 
-        error: null 
-    })),
-    on(PostActions.loadFeedSuccess, (state, { posts, count }) => ({
-        ...state,
-        items: posts,
-        totalCount: count,
-        loading: false
-    })),
-    on(PostActions.loadFeedFailure, (state, { error }) => ({
-        ...state,
-        loading: false,
-        error
-    })),
-    on(PostActions.createPost, (state) => ({ ...state, loading: true })),
-    on(PostActions.createPostSuccess, (state) => ({ ...state, loading: false })),
-)
+  on(PostActions.findAllPosts, (state) => ({ ...state, loading: true, error: null })),
+  on(PostActions.findAllPostsSuccess, (state, { list, count }) => ({
+    ...state,
+    list,
+    totalCount: count,
+    loading: false,
+  })),
+  on(PostActions.findAllPostsFailure, (state, { error }) => ({ ...state, error, loading: false })),
+
+  on(PostActions.loadMorePosts, (state) => ({ ...state, loadingMore: true, error: null })),
+  on(PostActions.loadMorePostsSuccess, (state, { list, count }) => ({
+    ...state,
+    list: [...state.list, ...list],
+    totalCount: count,
+    loadingMore: false,
+  })),
+  on(PostActions.loadMorePostsFailure, (state, { error }) => ({
+    ...state,
+    error,
+    loadingMore: false,
+  })),
+
+  // Create
+  on(PostActions.createPost, (state) => ({ ...state, loading: true, error: null })),
+  on(PostActions.createPostSuccess, (state) => ({ ...state, loading: false })), // Effect will reload feed
+  on(PostActions.createPostFailure, (state, { error }) => ({ ...state, loading: false, error }))
+);
