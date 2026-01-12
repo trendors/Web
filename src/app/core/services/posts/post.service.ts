b5503c460fa421f22ef9a49d2c9f86c6de95e7da
Igboanu Uchenna <igboanuuchenna@gmail.com>
2026-01-12 10:08:36 +0100

    added new apiUrl

--- src/app/core/services/posts/post.service.ts
+++ src/app/core/services/posts/post.service.ts
@@ -8,35 +8,38 @@ import {
   PagedListData,
   Post,
   LikePostDto,
+  FetchPostsResponse,
+  SaveOneResponse,
+  LikeResponse,
 } from '../../models/posts/post.model';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:3000/posts';
+  private apiUrl = 'https://trendors-main-service.onrender.com';
 
-  findAll(query: FetchPostDto): Observable<ApiResponse<PagedListData<Post>>> {
+  findAll(query: FetchPostDto): Observable<FetchPostsResponse> {
     const payload = {
       ...query,
       relations: query.relations ?? ['user', 'likes', 'comments', 'shares'],
     };
-    return this.http.post<ApiResponse<PagedListData<Post>>>(`${this.apiUrl}/findAll`, payload);
+    return this.http.post<FetchPostsResponse>(`${this.apiUrl}/findAll`, payload);
   }
 
-  loadMore(query: LoadMoreDto): Observable<ApiResponse<PagedListData<Post>>> {
+  loadMore(query: LoadMoreDto): Observable<FetchPostsResponse> {
     const payload = {
       ...query,
       relations: query.relations ?? ['user', 'likes', 'comments', 'shares'],
     };
-    return this.http.post<ApiResponse<PagedListData<Post>>>(`${this.apiUrl}/loadMore`, payload);
+    return this.http.post<FetchPostsResponse>(`${this.apiUrl}/loadMore`, payload);
   }
 
-  create(dto: CreatePostDto): Observable<ApiResponse<Post>> {
-    return this.http.post<ApiResponse<Post>>(`${this.apiUrl}`, dto);
+  create(dto: CreatePostDto): Observable<SaveOneResponse> {
+    return this.http.post<SaveOneResponse>(`${this.apiUrl}`, dto);
   }
 
-  likePost(dto: LikePostDto): Observable<ApiResponse<any>> {
-    return this.http.post<ApiResponse<any>>(`${this.apiUrl}/like`, dto);
+  likePost(dto: LikePostDto): Observable<LikeResponse> {
+    return this.http.post<LikeResponse>(`${this.apiUrl}/like`, dto);
   }
 }
