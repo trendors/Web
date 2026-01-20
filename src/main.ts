6781c3f9e934289a42e2d414c44703544a36e752
Igboanu Uchenna <igboanuuchenna@gmail.com>
2026-01-20 18:46:03 +0100

    added time ago package

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 // main.ts
-// import { provideTimeago } from 'ngx-timeago';
+import { provideTimeago } from 'ngx-timeago';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
