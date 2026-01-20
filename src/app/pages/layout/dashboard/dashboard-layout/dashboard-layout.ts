57e5bb4463ac241778fbf3d7b62c69f6e51c95e8
Igboanu Uchenna <igboanuuchenna@gmail.com>
2026-01-20 18:30:33 +0100

    dashboard

--- src/app/pages/layout/dashboard/dashboard-layout/dashboard-layout.ts
+++ src/app/pages/layout/dashboard/dashboard-layout/dashboard-layout.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { Store } from '@ngrx/store';
+import { selectCurrentUser } from '../../../../store/auth/shared state/auth.selector';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    RouterModule,
+    MatDividerModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
   templateUrl: './dashboard-layout.html',
   styleUrl: './dashboard-layout.scss',
 })
 export class DashboardLayout {
-
+  private store = inject(Store);
+  user$ = this.store.select(selectCurrentUser);
 }
