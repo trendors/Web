ed91b996bda39bdc4503bfa7ef2629f789a995aa
Igboanu Uchenna <igboanuuchenna@gmail.com>
2026-01-24 20:47:33 +0100

    added new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -37,6 +37,27 @@ export const routes: Routes = [
       import('./pages/auth/change-password/change-password').then((m) => m.ChangePassword),
   },
   {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./pages/layout/dashboard/dashboard-layout/dashboard-layout').then(
+        (m) => m.DashboardLayout,
+      ),
+    children: [
+      { path: '', redirectTo: 'profile', pathMatch: 'full' },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('./pages/layout/dashboard/profile/profile').then((m) => m.Profile),
+      },
+      {
+        path: 'change-password',
+        loadComponent: () =>
+          import('./pages/auth/change-password/change-password').then((m) => m.ChangePassword),
+      },
+    ],
+  },
+  {
     path: '**',
     redirectTo: 'login',
   },
