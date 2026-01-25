c9a2768ecbbcaf5172fee8887bbd8df3fad29ba2
sayil1 <sundaysayil4u@gmail.com>
2026-01-25 13:00:11 +0100

    add share to x

--- src/app/components/share-sheet/share-sheet.ts
+++ src/app/components/share-sheet/share-sheet.ts
@@ -3,11 +3,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import { PostService } from '../../core/services/posts/post.service';
-import { catchError, map, Observable } from 'rxjs';
+import { catchError, firstValueFrom, map, Observable } from 'rxjs';
 import { Actions } from '@ngrx/effects';
 import { UtilService } from '../../core/services/utility/utility.service';
 import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { AsyncPipe } from '@angular/common';
+import { Post } from '../../core/models/posts/post.model';
 
 
 
@@ -15,7 +16,7 @@ import { AsyncPipe } from '@angular/common';
   selector: 'app-share-sheet',
   imports: [
     MatListModule, MatIconModule, AsyncPipe
-],
+  ],
   templateUrl: './share-sheet.html',
   styleUrl: './share-sheet.scss',
 })
@@ -25,21 +26,33 @@ export class ShareSheet {
   constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: { post: any },
     private bottomSheetRef: MatBottomSheetRef<ShareSheet>) { }
 
-  selectedVersionId = 1;
-
- shareVersions$!: Observable<any[]>;
+  selectedPost!: string
+  shareVersions$!: Observable<any[]>;
+  trends$!: Observable<any[]>
 
   ngOnInit() {
     this.fetchAireWrite()
+    this.fetchXtrends()
+
   }
 
-  selectVersion(id: number) {
-    this.selectedVersionId = id;
+  selectVersion(post: string) {
+    this.selectedPost = post
+    console.log(this.selectedPost, "selected version")
   }
 
   fetchAireWrite() {
     let text = this.data.post.text
-    this.shareVersions$ = this.utilService.fetchAiRewrite(text);
+    this.shareVersions$ = this.utilService.fetchAiRewrite(text)
+
+  }
+
+  fetchXtrends() {
+    this.trends$ = this.utilService.fetchTrends().pipe(
+      map(data => {
+        return data.trends
+      })
+    )
   }
 
   copyLink() { }
@@ -47,5 +60,27 @@ export class ShareSheet {
   shareTo(url?: string) { }
 
   close() { }
+
+  async shareToX(post: any) {
+
+  const top4trends = await firstValueFrom(
+      this.trends$.pipe(
+        map((data: any[]) =>
+          data.slice(0, 4).map(item => item.name).join(', ')
+        )
+      )
+    );
+
+
+
+    const baseUrl = 'https://twitter.com/intent/tweet';
+    const params = new URLSearchParams({
+      text: `${this.selectedPost } \n ~ ${top4trends} \n https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png`
+    });
+
+    const shareUrl = `${baseUrl}?${params.toString()}`;
+        window.open(shareUrl, '_blank', 'width=550,height=420');
+
+  }
 
 }
