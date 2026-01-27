1d52e7d7f91c1ae3715d4edbeceb98570caf7028
Igboanu Uchenna <igboanuuchenna@gmail.com>
2026-01-27 17:27:45 +0100

    added the user services

---
+++ src/app/core/services/users/user.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { catchError, Observable, throwError } from 'rxjs';
+import { ApiResponse, UpdateUserDto, User } from '../../models/users/user.model';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/users`;
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.error('Userservice Error:', error);
+    let errorMessage: string;
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Network error: ${error.error.message}`;
+    } else if (error.error && error.error.message) {
+      errorMessage = Array.isArray(error.error.message)
+        ? error.error.message[0]
+        : error.error.message;
+    } else {
+      errorMessage = `Server returned code: ${error.status}, error message is: ${error.message}`;
+    }
+    return throwError(() => new Error(errorMessage));
+  }
+
+  updateUser(userId: number, updateData: UpdateUserDto): Observable<ApiResponse<User>> {
+    return this.http
+      .patch<ApiResponse<User>>(`${this.apiUrl}/${userId}`, updateData)
+      .pipe(catchError(this.handleError));
+  }
+
+  deleteUser(userId: number): Observable<ApiResponse<null>> {
+    return this.http
+      .delete<ApiResponse<null>>(`${this.apiUrl}/${userId}`)
+      .pipe(catchError(this.handleError));
+  }
+}
