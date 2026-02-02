e758acfe7b53b4cd00fdee0500e71c42157af835
Igboanu Uchenna <igboanuuchenna@gmail.com>
2026-02-02 19:15:25 +0100

    notification c page

---
+++ src/app/pages/notification/notification.ts
@@ -0,0 +1,39 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { take } from 'rxjs';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { selectCurrentUser } from '../../store/auth/shared state/auth.selector';
+import { NotificationActions } from '../../store/notification/notification.action';
+import {
+  selectAllNotifications,
+  selectNotificationsLoading,
+} from '../../store/notification/notification.selector';
+
+@Component({
+  selector: 'app-notifications-page',
+  imports: [AsyncPipe, DatePipe, MatListModule, MatButtonModule, RouterModule],
+  templateUrl: './notification.html',
+  styleUrl: './notification.scss',
+})
+export class NotificationsPage implements OnInit {
+  private store = inject(Store);
+
+  notifications$ = this.store.select(selectAllNotifications);
+  loading$ = this.store.select(selectNotificationsLoading);
+  user$ = this.store.select(selectCurrentUser);
+
+  ngOnInit() {
+    this.user$.pipe(take(1)).subscribe((u) => {
+      if (u && u.id) {
+        this.store.dispatch(NotificationActions.loadNotifications({ userId: u.id }));
+      }
+    });
+  }
+
+  markRead(notificationId: number, userId: number) {
+    this.store.dispatch(NotificationActions.markAsRead({ notificationId, userId }));
+  }
+}
