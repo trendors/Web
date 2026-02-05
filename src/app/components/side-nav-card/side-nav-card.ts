272b33f5d744d8625e6023892d59b310b0d3fa49
Igboanu Uchenna <igboanuuchenna@gmail.com>
2026-02-05 17:46:15 +0100

    hard to change it to this because the store was initialized BEFORE the constructor runs

--- src/app/components/side-nav-card/side-nav-card.ts
+++ src/app/components/side-nav-card/side-nav-card.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { take } from 'rxjs';
+import { selectCurrentUser } from '../../store/auth/shared state/auth.selector';
+import { NotificationActions } from '../../store/notification/notification.action';
+import { selectUnreadCount } from '../../store/notification/notification.selector';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-side-nav-card',
-  imports: [],
+  standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './side-nav-card.html',
   styleUrl: './side-nav-card.scss',
 })
-export class SideNavCard {
+export class SideNavCard implements OnInit {
+  // constructor(private router: Router, private store: Store) {} // Inject it here
 
-constructor(private router: Router) {} // Inject it here
+  private store = inject(Store);
+  private router = inject(Router);
+
+  unreadCount$ = this.store.select(selectUnreadCount);
+  currentUser$ = this.store.select(selectCurrentUser);
+
+  ngOnInit() {
+    this.currentUser$.pipe(take(1)).subscribe((user) => {
+      if (user?.id) {
+        this.store.dispatch(NotificationActions.loadNotifications({ userId: user.id }));
+      }
+    });
+  }
 
   routeTo(path: string) {
     // Implement your routing logic here, e.g. using Angular's Router
