2503b6d2c039cd130e8c30888da6e6e3b810d6a5
Igboanu Uchenna <igboanuuchenna@gmail.com>
2026-02-25 16:29:52 +0100

    backend response is in an array so i corrected it

--- src/app/core/services/campaign/campaign.service.ts
+++ src/app/core/services/campaign/campaign.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
-import { Observable } from 'rxjs';
-import { Campaign } from '../../models/campaign/campaign.model';
+import { map, Observable } from 'rxjs';
+import { ApiResponse, Campaign } from '../../models/campaign/campaign.model';
 
 @Injectable({ providedIn: 'root' })
 export class CampaignService {
@@ -10,6 +10,8 @@ export class CampaignService {
   private apiUrl = `${environment.apiUrl}/campaign`;
 
   getCampaigns(userId: string | number): Observable<Campaign[]> {
-    return this.http.get<Campaign[]>(`${this.apiUrl}?creator_id=${userId}`);
+    return this.http
+      .get<ApiResponse<{ list: Campaign[] }>>(`${this.apiUrl}?creator_id=${userId}`)
+      .pipe(map((res) => res.data?.list || []));
   }
 }
