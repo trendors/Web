4ae7868fdd08bf86b57d2dd4f9735dc279a94781
Igboanu Uchenna <igboanuuchenna@gmail.com>
2026-03-21 12:11:49 +0100

    added invitation services

--- src/app/core/services/invitation/invitation.service.ts
+++ src/app/core/services/invitation/invitation.service.ts
@@ -12,12 +12,30 @@ export class InvitationService {
 
   getMyInvitations(userId: number): Observable<Invitation[]> {
     const url = `${this.apiUrl}/all-invitations/${userId}`;
-    console.log('getMyInvitations URL', url);
     return this.http.get<ApiResponse<{ list: Invitation[] }>>(url).pipe(
       map((r) => {
-        console.log('API response', r);
         return r.data?.list || [];
       }),
     );
+  }
+
+  getCampaignInvites(campaignId: number): Observable<Invitation[]> {
+    return this.http
+      .get<ApiResponse<Invitation[]>>(`${this.apiUrl}/campaign/${campaignId}/all-invites`)
+      .pipe(
+        map((r) => {
+          return Array.isArray(r.data) ? r.data : [];
+        }),
+      );
+  }
+
+  respondInvitation(invitationId: number, userId: number, status: 'Accepted' | 'Declined') {
+    return this.http.patch<ApiResponse<any>>(`${this.apiUrl}/${invitationId}/respond/${userId}`, {
+      status,
+    });
+  }
+
+  revokeInvitation(invitationId: number) {
+    return this.http.delete<ApiResponse<any>>(`${this.apiUrl}/${invitationId}/revoke`);
   }
 }
