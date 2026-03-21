1020921ad1e1aab4b45ba20f42480ede4367e1f8
Igboanu Uchenna <igboanuuchenna@gmail.com>
2026-03-21 12:15:03 +0100

    added the sucessful flow reduce

--- src/app/store/campaign/campaign.reducer.ts
+++ src/app/store/campaign/campaign.reducer.ts
@@ -1,28 +1,40 @@
-import { createReducer, on } from "@ngrx/store";
-import { Campaign } from "../../core/models/campaign/campaign.model";
-import { CampaignActions } from "./campaign.action";
+import { createReducer, on } from '@ngrx/store';
+import { Campaign } from '../../core/models/campaign/campaign.model';
+import { CampaignActions } from './campaign.action';
 
 export const campaignFeatureKey = 'campaign';
 
 export interface CampaignState {
-    list: Campaign[];
-    isLoading: boolean;
-    error: string | null;
-};
+  list: Campaign[];
+  isLoading: boolean;
+  error: string | null;
+}
 
 export const initialState: CampaignState = {
-    list: [],
-    isLoading: false,
-    error: null,
+  list: [],
+  isLoading: false,
+  error: null,
 };
 
 export const campaignReducer = createReducer(
-    initialState,
-    on(CampaignActions.loadCampaigns, (state) => ({ ...state, isLoading: true, error: null})),
-    on(CampaignActions.loadCampaignsSuccess, (state, { list }) => ({ ...state, isLoading:false, list })),
-    on(CampaignActions.loadCampaignsFailure, (state, {error}) => ({ ...state, error })),
+  initialState,
+  on(CampaignActions.loadCampaigns, (state) => ({ ...state, isLoading: true, error: null })),
+  on(CampaignActions.loadCampaignsSuccess, (state, { list }) => ({
+    ...state,
+    isLoading: false,
+    list,
+  })),
+  on(CampaignActions.loadCampaignsFailure, (state, { error }) => ({ ...state, error })),
 
-    on(CampaignActions.createCampaign, (state) => ({ ...state, isLoading: true, error: null })),
-    on(CampaignActions.createCampaignSuccess, (state, { message }) => ({ ...state, isLoading: false })),
-    on(CampaignActions.createCampaignFailure, (state, { error }) => ({ ...state, isLoading: false, error })),
+  on(CampaignActions.createCampaign, (state) => ({ ...state, isLoading: true, error: null })),
+  on(CampaignActions.createCampaignSuccess, (state, { campaign }) => ({
+    ...state,
+    isLoading: false,
+    campaign,
+  })),
+  on(CampaignActions.createCampaignFailure, (state, { error }) => ({
+    ...state,
+    isLoading: false,
+    error,
+  })),
 );
