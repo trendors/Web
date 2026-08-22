1ea1c9b9c96525871278c21ebde42cdb053eb0f2
sayil1 <sundaysayil4u@gmail.com>
2026-08-22 13:48:56 +0100

    fix routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'home/view-campaign/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: 'home/post/:id',
     renderMode: RenderMode.Server, // This forces SSR instead of Prerendering
   },
